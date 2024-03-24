7129251373bf99325a0a8cd1e3b7b7d67ad9271b
MD. Famidul Islam Pranto <119770294+FI-Pranto@users.noreply.github.com>
2024-03-24 21:52:31 +0600

    Add files via upload
    codes

---
+++ div4_q1.py
@@ -0,0 +1,11 @@
+i=1
+t=int(input())
+while i<=t:
+    x=input()
+    if x[0]=='a' or x[1]=='b' or x[2]=='c':
+        print("YES")
+    else:
+        print("NO")
+    i+=1
+
+
---
+++ div4_q2.py
@@ -0,0 +1,10 @@
+i=1
+t=int(input())
+while i<=t :
+    n=int(input())
+    list=[]
+    for j in range(n):
+        ele = int(input())
+        list.append(ele)
+    print(list)
+    i+=1
